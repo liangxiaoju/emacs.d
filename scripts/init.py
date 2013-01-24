aac337fc23a7da3e6664074c7255f0aa2dc24e02
liangxiaoju <liangxiaoju@gmail.com>
2013-01-24 15:34:49 +0800

    modify init script

--- scripts/init.py
+++ scripts/init.py
@@ -3,9 +3,10 @@
 import os
 
 cmds = ["git submodule update --init",
-        "git checkout -B purcell",
         "git remote add purcell https://github.com/purcell/emacs.d.git",
         "git fetch purcell",
+        "git checkout purcell/master",
+        "git checkout -b purcell",
         "git branch --set-upstream-to=purcell/master purcell",
         "git checkout master"]
 
