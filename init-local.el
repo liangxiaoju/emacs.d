3504b89b71b78fb7207529c3a2b4795007ef3be2
liangxiaoju <liangxiaoju@gmail.com>
2013-04-01 16:18:26 +0800

    init-local.el

--- init-local.el
+++ init-local.el
@@ -3,10 +3,27 @@
         (awk-mode . "awk")
         (other . "linux")))
 
+;;(setq c-auto-newline t)
+
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
 (setq-default c-basic-offset 4)
 ;; overwrite shutcut key
 (global-set-key "\C-co" 'overwrite-mode)
+;;mouse auto avoid
+(mouse-avoidance-mode 'animate)
+;;show line number
+;;(global-linum-mode t)
+;;no temp file
+;;(setq-default make-backup-files nil)
+;;frame title
+;;(setq frame-title-format "emacs@%b")
+;;(setq kill-ring-max 512)
+
+;;===================for python=======================
+;;dont guess, use 4
+;;(setq python-guess-indent nil)
+;;(setq python-indent 4)
 
 ;;====================window==========================
 (defun select-next-window ()
@@ -27,6 +44,9 @@
 (global-set-key [S-down] 'windmove-down)
 ;;====================================================
 
+(require 'sr-speedbar nil t)
+(global-set-key [f8] 'sr-speedbar-toggle)
+(setq speedbar-tag-hierarchy-method '(speedbar-simple-group-tag-hierarchy))
 
 (require 'init-w3m nil t)
 
