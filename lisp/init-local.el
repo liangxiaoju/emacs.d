431deef0c2064eebd3a70a46e384860d8f5d9f5e
xiaoju.liang <xiaoju.liang@tcl.com>
2017-06-16 16:20:40 +0800

    change in init-local/init-w3m

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,10 +1,16 @@
+;;; init-local.el --- local configures
+;;; Commentary:
+;;; Code:
+
+;;----------------------------------------------------------------------------
+;; misc
+;;----------------------------------------------------------------------------
 (setq c-default-style
       '((java-mode . "java")
         (awk-mode . "awk")
         (other . "linux")))
 
 ;;(setq c-auto-newline t)
-
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq-default c-basic-offset 4)
@@ -19,14 +25,12 @@
 ;;frame title
 ;;(setq frame-title-format "emacs@%b")
 ;;(setq kill-ring-max 512)
-;; set default font size
-;; (set-face-attribute 'default nil :height 130)
-;; or set font size in custom-set-faces as below
-;;(custom-set-faces '(default ((t (:height 130 :font "Inconsolata-14")))))
-;;(set-frame-font "inconsolata-14")
-;;(add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
 
-;;===================for terminal=======================
+;;----------------------------------------------------------------------------
+;; theme
+;;----------------------------------------------------------------------------
+(when (maybe-require-package 'color-theme-sanityinc-solarized)
+  (setq-default custom-enabled-themes '(sanityinc-solarized-dark)))
 ;;set background-color to nil, so it will use terminal's background color
 (add-hook 'after-make-console-frame-hooks
           (lambda () (set-frame-parameter nil 'background-color nil)))
@@ -34,19 +38,23 @@
           (lambda () (if (not window-system)
                     (set-frame-parameter nil 'background-color nil))))
 
-;;===================for python=======================
+;;----------------------------------------------------------------------------
+;; python
+;;----------------------------------------------------------------------------
 ;;dont guess, use 4
 ;;(setq python-guess-indent nil)
 ;;(setq python-indent 4)
 
-;;====================window==========================
+;;----------------------------------------------------------------------------
+;; window
+;;----------------------------------------------------------------------------
 (defun select-next-window ()
-  "Switch to the next window"
+  "Switch to the next window."
   (interactive)
   (select-window (next-window)))
 
 (defun select-previous-window ()
-  "Switch to the previous window"
+  "Switch to the previous window."
   (interactive)
   (select-window (previous-window)))
 
@@ -56,50 +64,92 @@
 (global-set-key [S-left] 'windmove-left)
 (global-set-key [S-up] 'windmove-up)
 (global-set-key [S-down] 'windmove-down)
-;;====================================================
 
-(require 'sr-speedbar nil t)
-(global-set-key [f8] 'sr-speedbar-toggle)
-(setq speedbar-tag-hierarchy-method '(speedbar-simple-group-tag-hierarchy))
+;;----------------------------------------------------------------------------
+;; w3m
+;;----------------------------------------------------------------------------
+;;(require 'init-w3m nil t)
 
-(require 'xcscope)
-(cscope-setup)
-(setq cscope-use-relative-paths nil)
-(add-hook 'java-mode-hook (function cscope-minor-mode))
-(setq cscope-indexer-suffixes '("*.[chly]"
-                                "*.[ch]xx"
-                                "*.[ch]pp"
-                                "*.cc"
-                                "*.hh"
-                                "*.java"))
+;;----------------------------------------------------------------------------
+;; flycheck
+;;----------------------------------------------------------------------------
+(setq flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
 
-(defun cscope-append-directory (directory)
-  (interactive "DCscope append Directory: ")
-  (let (src dst)
-    (setq src (concat (cscope-canonicalize-directory directory) cscope-index-file))
-    (setq dst (concat (cscope-search-directory-hierarchy default-directory)
-                      cscope-index-file))
-    (message (concat src " -> " dst))
-    (call-process-shell-command
-     (concat "cd " directory " && " (cscope-make-index-command directory t))
-     nil nil)
-    (call-process-shell-command
-     (concat "cat " src " >> " dst "\n")
-     nil nil)
-    (delete-file src))
+;;----------------------------------------------------------------------------
+;; evil
+;;----------------------------------------------------------------------------
+(when (maybe-require-package 'evil)
+  (setq-default evil-default-state 'emacs)
+  (setq-default evil-toggle-key "C-c vi")
+  (evil-mode 0))
+
+;;----------------------------------------------------------------------------
+;; csharp-mode
+;;----------------------------------------------------------------------------
+(when (maybe-require-package 'csharp-mode)
+  (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+  (setq auto-mode-alist (append '(("\\.cs$" . csharp-mode)) auto-mode-alist)))
+
+;;----------------------------------------------------------------------------
+;; imenu
+;;----------------------------------------------------------------------------
+(global-set-key [f8] 'imenu)
+
+;;----------------------------------------------------------------------------
+;; xcscope
+;;----------------------------------------------------------------------------
+;; use 'cscope-minor-mode to start xcscope
+;;(maybe-require-package 'xcscope)
+(after-load 'xcscope
+  (cscope-setup)
+  (setq cscope-use-relative-paths nil)
+  (setq cscope-indexer-suffixes '("*.[chly]"
+                                  "*.[ch]xx"
+                                  "*.[ch]pp"
+                                  "*.cc"
+                                  "*.hh"
+                                  "*.java"))
+  (defun cscope-append-directory (directory)
+    (interactive "DCscope append Directory: ")
+    (let (src dst)
+      (setq src (concat (cscope-canonicalize-directory directory) cscope-index-file))
+      (setq dst (concat (cscope-search-directory-hierarchy default-directory)
+                        cscope-index-file))
+      (message (concat src " -> " dst))
+      (call-process-shell-command
+       (concat "cd " directory " && " (cscope-make-index-command directory t))
+       nil nil)
+      (call-process-shell-command
+       (concat "cat " src " >> " dst "\n")
+       nil nil)
+      (delete-file src))
+    )
+  (define-key cscope-minor-mode-keymap "\C-cs+"  'cscope-append-directory)
+  (add-hook 'java-mode-hook (function cscope-minor-mode))
   )
 
-(global-set-key "\C-cs+" 'cscope-append-directory)
+;;----------------------------------------------------------------------------
+;; gtags
+;;----------------------------------------------------------------------------
+;;(maybe-require-package 'counsel-gtags)
+(after-load 'counsel-gtags
+  (setq-default counsel-gtags-use-suggested-key-map t)
+  (add-hook 'java-mode-hook 'counsel-gtags-mode)
+  (add-hook 'c++-mode-hook 'counsel-gtags-mode)
+  (add-hook 'c-mode-hook 'counsel-gtags-mode))
 
-(require 'init-w3m nil t)
-
-(setq evil-default-state 'emacs)
-(setq evil-toggle-key "C-c vi")
-(require 'evil)
-(evil-mode 1)
-
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist
-      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+;;----------------------------------------------------------------------------
+;; email
+;;----------------------------------------------------------------------------
+;; for SMTP send mail, customize:
+;;'(send-mail-funtion '(smtpmail-send-it))
+;;'(smtpmail-smtp-server "SMTP MAIL HOST")
+;;'(smtpmail-smtp-service PORT)
+;;'(user-full-name "")
+;;'(user-mail-address "")
+;; for POP rmail, customize:
+;;'(rmail-preserve-inbox t)
+;;'(rmail-primary-inbox-list '("pop://USE:PASSWD@HOST:PORT"))
 
 (provide 'init-local)
+;;; init-local.el ends here
--- lisp/init-w3m.el
+++ lisp/init-w3m.el
@@ -1,19 +1,27 @@
-(setq w3m-default-display-inline-images nil)
-(setq w3m-use-toolbar nil)
+;;; init-w3m.el --- configure for w3m
+;;; Commentary:
+;;; Code:
 
-;;add search engines
-(eval-after-load "w3m-search"
-  '(add-to-list 'w3m-search-engine-alist
-                '("baidu"
-                  "http://www.baidu.com/s?wd=%s"
-                   utf-8)))
+(when (maybe-require-package 'w3m)
+  (setq w3m-default-display-inline-images nil)
+  (setq w3m-use-toolbar nil)
 
-(setq w3m-search-default-engine "google")
-(setq w3m-home-page "http://www.google.com")
-(setq w3m-make-new-session t)
-(setq w3m-use-cookies t)
+  ;;add search engines
+  (eval-after-load "w3m-search"
+    '(add-to-list 'w3m-search-engine-alist
+                  '("baidu"
+                    "http://www.baidu.com/s?wd=%s"
+                    utf-8)))
 
-(setq browse-url-browser-function 'w3m-browse-url)
+  (setq w3m-search-default-engine "baidu")
+  (setq w3m-home-page "http://www.baidu.com")
+  (setq w3m-make-new-session t)
+  (setq w3m-use-cookies t)
+
+  (setq browse-url-browser-function 'w3m-browse-url)
+  )
+
 (global-set-key "\C-cw" 'browse-url-at-point)
 
 (provide 'init-w3m)
+;;; init-w3m ends here
